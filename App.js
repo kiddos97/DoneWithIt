7816e83d67dceac8b622410b473d78ed74eb2cb0
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-25 19:05:23 -0600

    update

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView} from 'react-native'
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableWithoutFeedback} from 'react-native'
 export default function App() {
 
   console.log('App executed')
@@ -7,8 +7,8 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>I am home
-      </Text>
+      <Text>I am home</Text>
+      <TouchableWithoutFeedback onPress={() => console.log('image tapped')}><Image source={require('./assets/favicon.png')}/></TouchableWithoutFeedback>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -18,5 +18,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'dodgerblue',
+    justifyContent : "center",
+    alignItems: "center"
+
   },
 });
