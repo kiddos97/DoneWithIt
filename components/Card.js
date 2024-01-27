27c0c04bd9a9b37d11a4aeb3a68a053b4d76e7b9
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-27 15:11:40 -0600

    adding card and list

--- components/Card.js
+++ components/Card.js
@@ -5,8 +5,10 @@ const Card = ({title, subTitle, image}) => {
   return (
    <View style={styles.card}>
     <Image style={styles.image} source={image}/>
-    <Text>{title}</Text>
-    <Text>{subTitle}</Text>
+    <View style={styles.textContainer}> 
+    <Text style={styles.title}>{title}</Text>
+    <Text style={styles.subTitle}>{subTitle}</Text>
+    </View>
    </View>
   )
 }
@@ -16,11 +18,22 @@ const styles = StyleSheet.create({
     card:{
         borderRadius: 15,
         backgroundColor: '#fff',
-        marguinBottom: 20
+        marguinBottom: 20,
+        overflow:'hidden'
     },
     image:{
         width:'100%',
-        height:200
+        height:500,
+    },
+    textContainer:{
+        padding: 20
+    },
+    title:{
+        marginBottom: 7
+    },
+    subTitle:{
+        color:'green',
+        fontWeight: 'bold'
     }
 })
 
