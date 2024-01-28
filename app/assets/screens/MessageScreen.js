62ec1e139af79407341d41f2cd9fcd4eb6d54db8
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-28 00:27:07 -0600

    updating

--- app/assets/screens/MessageScreen.js
+++ app/assets/screens/MessageScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FlatList, SafeAreaView, StyleSheet, Platform, StatusBar,View } from 'react-native'
+import { FlatList, SafeAreaView, StyleSheet, Platform, StatusBar,View ,Text} from 'react-native'
 import ListItem from '../../../components/ListItem'
 
 import ListitemSeparator from '../../../components/ListitemSeparator'
@@ -20,25 +20,51 @@ const message =[ {
 const MessageScreen = () => {
   return (
     <SafeAreaView style={styles.screen}>
- <FlatList 
-    data={message}
-    keyExtractor={(message) => message.id.toString()}
-    renderItem={({ item }) => 
-    <ListItem
-    title={item.title}
-    subTitle={item.description}
-    image={item.image}/>}
-    ItemSeparatorComponent={ListitemSeparator}
-  />
-    
+      <FlatList
+        data={message}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+            onPress={() => console.log('message selected', item)}
+            renderRightActions={() => (
+              <View
+                style={{
+                  backgroundColor: 'red',
+                  width: 70,
+                  height: '100%', // Adjust height as needed
+                }}
+              ></View>
+            )}
+            renderLeftActions={() => (
+            <View
+            
+            style={{
+              width:70,
+              backgroundColor:'blue',
+              height:'100%',
+              justifyContent:'center',
+              alignItems:'center'
+            }}
+            ><Text style={styles.text}>Archive</Text></View>)}
+          />
+          
+        )}
+        ItemSeparatorComponent={ListitemSeparator} // Make sure ListitemSeparator is defined or import correctly
+      />
     </SafeAreaView>
- 
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
     screen:{
         paddingtop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+    },
+    text:{
+    color:'#fff',
+    fontWeight:400
     }
 })
 
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,15 +1,30 @@
 import React from 'react'
-import { View, StyleSheet, Image,Text } from 'react-native'
+import { View, StyleSheet, Image,Text, TouchableHighlight } from 'react-native'
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-const ListItem = ({ title, subTitle, image}) => {
+const ListItem = ({ title, subTitle, image, onPress, renderRightActions,renderLeftActions}) => {
+
+
   return (
-   <View style={styles.container}>
-    <Image style={styles.image} source={image} />
-    <View>
-        <Text style={styles.title}>{title}</Text>
-        <Text  style={styles.subTitle} >{subTitle}</Text>
-    </View>
-   </View>
+    <GestureHandlerRootView> 
+    <Swipeable renderLeftActions={renderLeftActions} renderRightActions={renderRightActions}> 
+    <TouchableHighlight 
+    underlayColor="grey"
+    onPress={onPress}>
+        <View style={styles.container}>
+         <Image style={styles.image} source={image} />
+         <View>
+             <Text style={styles.title}>{title}</Text>
+             <Text  style={styles.subTitle} >{subTitle}</Text>
+         </View>
+        </View>
+    </TouchableHighlight>
+    </Swipeable>
+    </GestureHandlerRootView>
+   
+    
+ 
   )
 }
 
