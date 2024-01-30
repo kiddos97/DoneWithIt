9b46e0c80d9f618ca2c628e95d29af071019a81b
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-29 23:11:35 -0600

    update: Organizing components

--- components/Menu/Message.js
+++ components/Menu/Message.js
@@ -1,10 +1,10 @@
 export const message =[
     {
-      id: 1, title: 't1', description: 'D1',image:require('../app/assets/person.jpg')
+      id: 1, title: 't1', description: 'D1',image:require('../../app/assets/person.jpg')
   },
   
   {
-      id: 2, title: 't2', description: 'D2',image:require('../app/assets/person.jpg')
+      id: 2, title: 't2', description: 'D2',image:require('../../app/assets/person.jpg')
   },
   
   ]
