018f34cfd49c874c09334399acf94c6499616efd
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-29 22:41:19 -0600

    updating

--- app/assets/screens/LoginScreen.js
+++ app/assets/screens/LoginScreen.js
@@ -4,41 +4,50 @@ import { Image, SafeAreaView, StyleSheet, View } from 'react-native'
 import AppTextInput from '../../../components/AppTextInput'
 import AppButton from '../../../components/AppButton'
 
+import { Formik } from 'formik'
+
 const LoginScreen = () => {
         
-    const [email, setEmail] = [] = useState();
-
-    const [password, setPassword] = [] = useState();
-
-
+  
 
   return (
-            <SafeAreaView>
+            <SafeAreaView >
                 <Image 
                 style={styles.logo} 
                 source={require('../logo.png')}
                 />
-                <View style={styles.Appbutton}>
-                    <AppTextInput
-                        autoCapitalize="none"
-                        autoCorrect={false}
-                        icon='email'
-                        keyboardType="email-address"
-                        placeholder="Email"
-                    />
-                </View>
-                <View style={styles.Appbutton}>
-                    <AppTextInput
-                      autoCapitalize="none"
-                      autoCorrect={false}
-                      icon='lock'
-                      keyboardType="email-address"
-                      placeholder="Password"
-                      secureTextEntry
-                      textContentType="password"
-                    />
-                </View>
-                <AppButton title="Login" onPress={() => console.log()} />
+                <Formik
+                initialValues={{email:'',password:''}}
+                onSubmit={(values) => console.log(values)}
+                >
+                    {({ handleChange, handleSubmit}) => (
+                        <>
+                           <View style={styles.screen}>
+                        <AppTextInput
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            icon='email'
+                            keyboardType="email-address"
+                            onChangeText={handleChange("email")}
+                            placeholder="Email"
+                        />
+                        <AppTextInput
+                          autoCapitalize="none"
+                          autoCorrect={false}
+                          icon='lock'
+                          keyboardType="email-address"
+                          onChangeText ={ handleChange('password') }
+                          placeholder="Password"
+                          secureTextEntry
+                          textContentType="password"
+                        />
+                    </View>
+                <AppButton title="Login" onPress={handleSubmit} />
+                        </>
+
+                    )}
+                </Formik>
+                 
             </SafeAreaView>
    
   )
@@ -47,13 +56,12 @@ const LoginScreen = () => {
 const styles = StyleSheet.create({
     logo:{
         width:100,
-        height: 100,
+        height: 150,
         alignSelf:'center',
         marginTop:50,
         marginBottom: 20
     },
-    Appbutton:{
-        width:'100%',
+    screen:{
         padding:10,
       },
 })
