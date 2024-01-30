47d21dd688048265c2f501f49aefb8bc4b2d21ce
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-30 00:35:31 -0600

    updating

--- app/assets/screens/NewListingScreen.js
+++ app/assets/screens/NewListingScreen.js
@@ -1,28 +1,67 @@
-import React from 'react'
-import { SafeAreaView, View, StyleSheet } from 'react-native'
+import React,{ useState}  from 'react'
+import { SafeAreaView, View, StyleSheet,Text } from 'react-native'
 import AppTextInput from '../../../components/App/AppTextInput'
 import AppPicker from '../../../components/App/AppPicker'
 import AppButton from '../../../components/App/AppButton'
+import { categories } from '../../../components/Menu/CategoriesList'
+
+import { Formik } from 'formik'
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({ // Yup form validation
+
+    title: Yup.string().required().min(1).label("Title"),
+    price: Yup.number().required().min(1).max(10000).label("Price"),
+    category: Yup.object().required().nullable().label('Category'),
+    description: Yup.string().label("Description")
+})
 
 const NewListingScreen = () => {
+    const [category, setCategory] = useState('');
   return (
     
     <SafeAreaView>
-        <View style={styles.container} >
-            <AppTextInput placeholder="Title"/>
-            <AppTextInput placeholder="Price"/>
-            <AppPicker 
-            placeholder="Category"
-            />
-            <AppTextInput placeholder="Description"/>
-            <AppButton title="POST"/>
-        </View>
+        <Formik
+          initialValues={{title:'',price:'',category:null,description:''}}
+          onSubmit={(values) => console.log(values)}
+          validationSchema={validationSchema}
+        >
+             {({ handleChange, handleSubmit, errors, setFieldTouched, touched,}) => (
+                <>
+                <View style={styles.container} >
+                <AppTextInput 
+
+                onBlur={() => setFieldTouched('title')}
+                onChangeText={handleChange("title")}
+                placeholder="Title"/>
+                { touched.title && <Text style={{color:'red'}}>{errors.title}</Text>}
+                <AppTextInput 
+                keyboardType="numeric"
+                placeholder="Price"/>
+                { touched.price && <Text style={{color:'red'}}>{errors.price}</Text>}
+                <AppPicker 
+                selectedItem={category}
+                onSelectItem={(item) => setCategory(item)}
+                items={categories}
+                placeholder="Category"
+                />
+                { touched.category && <Text style={{color:'red'}}>{errors.category}</Text>}
+                <AppTextInput placeholder="Description"/>
+                { touched.description && <Text style={{color:'red'}}>{errors.description}</Text>}
+                <AppButton title="POST" onPress={handleSubmit}/>
+            </View>
+                </>
+             )}
+
+        </Formik>
+       
     </SafeAreaView>
   )
 }
 const styles = StyleSheet.create({
     container:{
-        padding:10
+        padding:10,
+        paddingTop:30
     }
 })
 export default NewListingScreen
---
+++ components/Menu/CategoriesList.js
@@ -0,0 +1,8 @@
+
+export const categories = [
+
+    {label: "Furniture", value:1},
+    {label: "Clothing", value:2},
+    {label: "Cameras", value:3},
+    
+  ]
