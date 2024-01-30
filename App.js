9edf75701621a3738b403ac80ca67788dc5b8b35
Emmanuel <osaroimarhiagbe@gmail.com>
2024-01-29 19:47:25 -0600

    updating

--- App.js
+++ App.js
@@ -2,18 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, SafeAreaView, TouchableWithoutFeedback, Button,Alert, Platform, Dimensions, TextInput} from 'react-native'
 import { useState} from 'react'
 import { useDeviceOrientation } from "@react-native-community/hooks"
+
 import WelcomeScreen from './app/assets/screens/WelcomeScreen';
 import ViewImageScreen from './app/assets/screens/ViewImageScreen';
 import ButtonExcerise from './app/assets/screens/ButtonExcerise';
-
-import Card from './components/Card';
-import ListingDetails from './app/assets/screens/ListingDetails';
-import MessageScreen from './app/assets/screens/MessageScreen';
 import AccountScreen from './app/assets/screens/AccountScreen';
 import ListingScreen from './app/assets/screens/ListingScreen';
+import MessageScreen from './app/assets/screens/MessageScreen';
+import LoginScreen from './app/assets/screens/LoginScreen';
+
+
+
+{/** Components */}
+import Card from './components/Card';
+import ListingDetails from './app/assets/screens/ListingDetails';
 import AppTextInput from './components/AppTextInput';
 import AppPicker from './components/AppPicker';
-import LoginScreen from './app/assets/screens/LoginScreen';
+
 
 
 
@@ -25,7 +30,8 @@ export default function App() {
   
   return (
 
-    <LoginScreen/>
+   <LoginScreen/>
+    
   );
 
   
