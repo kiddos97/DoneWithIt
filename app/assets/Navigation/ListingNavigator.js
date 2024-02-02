850b3ab257af5a0fe61332001920c257f917ff31
Emmanuel <osaroimarhiagbe@gmail.com>
2024-02-02 01:21:32 -0600

    adding icon

--- app/assets/Navigation/ListingNavigator.js
+++ app/assets/Navigation/ListingNavigator.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs" 
-
+import { TouchableOpacity, View, StyleSheet } from 'react-native';
 
 import ListingScreen from '../screens/ListingScreen';
 import NewListingScreen from '../screens/NewListingScreen';
@@ -18,31 +18,51 @@ const ListingNavigator = () => {
     const Tab = createBottomTabNavigator();
 
 
+
   return (
 
-    <Tab.Navigator initialRouteName="Feed">
-    <Tab.Screen 
-    name="Feed" 
-    color={colors.dark}
-    component={ListingScreen}
-    options={({ route }) => ({
-      headerShown: false,
-      tabBarLabel: 'Feed',
-      tabBarIcon: ({ color, size }) => (
-        <MaterialCommunityIcons 
-          name="home" 
-          color={route.name === 'Feed' ? colors.dark : color} // Change 'blue' to the desired color
-          size={size} 
-        />
-      ),
-      headerShown:false,
-    })}
-    />
-    <Tab.Screen
-    name="New Listing "
-    component={NewListingScreen}
-    options={{headerShown:false}}
-    />
+    <Tab.Navigator
+    initialRouteName="Feed"
+    screenOptions={{
+      tabBarActiveTintColor:colors.danger
+        }}
+    >
+  
+      <Tab.Screen
+      name="Feed"
+      color={colors.dark}
+      component={ListingScreen}
+      options={({ route }) => ({
+        headerShown: false,
+        tabBarLabel: 'Feed',
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons
+            name="home"
+            color={route.name === 'Feed' ? colors.danger : color} // Change 'blue' to the desired color
+            size={size}
+          />
+        ),
+        headerShown:false,
+      })}
+      />
+      <Tab.Screen
+      name="Listing"
+      component={NewListingScreen}
+      options={({ route }) => ({
+        headerShown: false,
+        tabBarLabel:'Listing',
+        tabBarIcon: ({ color,size }) => (
+          <View style={styles.iconContainer}>
+          <MaterialCommunityIcons
+            name="plus-circle"
+            color={route.name === 'Listing' ? colors.light : color } // Change 'blue' to the desired color
+            size={20}
+          />
+          </View>
+        ),
+        headerShown:false,
+      })}
+      />
     <Tab.Screen 
     name="AccountNav" 
     component={AccountNavigator} 
@@ -52,7 +72,7 @@ const ListingNavigator = () => {
       tabBarIcon: ({ color,size }) => (
         <MaterialCommunityIcons 
           name="account" 
-          color={route.name === 'AccountNav' ? colors.dark : colors.light } // Change 'blue' to the desired color
+          color={route.name === 'AccountNav' ? colors.danger : colors.light } // Change 'blue' to the desired color
           size={size} 
         />
       ),
@@ -62,5 +82,14 @@ const ListingNavigator = () => {
     </Tab.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  iconContainer:{
+    padding:8,
+    margintop:-10,
+    backgroundColor:colors.danger,
+    borderRadius: 35
+  }
+})
 
 export default ListingNavigator
